c3032e4ed283612d6345d54eac3601827b2c7897
karnkittik <karnkitti_tang@hotmail.com>
2020-12-15 17:48:55 +0700

    Add Menu Sidebar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React, { useEffect, useState } from "react";
-import "./Reset.css";
+import React, { useState } from "react";
+import { Layout } from "antd";
+import Sider from "./Sider";
 import "./App.less";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Redirect,
-  Route,
-  useHistory,
-} from "react-router-dom";
-import Home from "./pages/home";
 
+const { Content } = Layout;
 const App = () => {
-  let history = useHistory();
+  const style = {
+    fontSize: "30px",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "10px 0",
+  };
+
+  const components = {
+    1: <div style={style}>อุบัติเหตุทั้งหมด</div>,
+    2: <div style={style}>อาการง่วงนอนทั้งหมด</div>,
+    3: <div style={style}>ภาพรวมอุบัติเหตุ</div>,
+    4: <div style={style}>ภาพรวมอุบัติเหตุรายวัน</div>,
+  };
+
+  const [render, updateRender] = useState(1);
+
+  const handleMenuClick = (menu) => {
+    updateRender(menu.key);
+  };
+
   return (
-    <div>
-      <Router history={history}>
-        <Switch>
-          <Route path="/" exact render={Home} />
-        </Switch>
-      </Router>
+    <div className="App">
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider handleClick={handleMenuClick} />
+        <Layout>
+          <Content>{components[render]}</Content>
+        </Layout>
+      </Layout>
     </div>
   );
 };
-
 export default App;
